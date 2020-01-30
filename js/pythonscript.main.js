01ecb1911c5a322db3d7766bd96e9c13b500d3d8
iLoveGachimuchi <rul3iofu1s7@outlook.com>
2020-01-30 15:23:41 +0300

    first steps

--- js/pythonscript.main.js
+++ js/pythonscript.main.js
@@ -5,7 +5,11 @@ function isNativeObject(object) {
     return Object.prototype.toString.call(object) === '[object Object]';
 }
 
-//runner = 1 (js compiled in js) || 0 (work as native),
+/**
+* runner: 1 - compiled in js
+*         0 - work as native
+* spaces: tab size
+**/
 class pythonscript_configuration {
     constructor() {
         this.PS_configuration = 1;
@@ -13,6 +17,17 @@ class pythonscript_configuration {
 
     set_default_params() {
         this.runner = 0;
+        this.spaces = 2;
+    }
+
+    get_spaces() {
+        return this.spaces;
+    }
+
+    set_spaces(number) {
+        this.spaces = (number > 0 && number < 9) ? number : -1;
+        if (this.spaces == -1)
+            throw ("Tab size must be 1...8")
     }
 
     get_runner() {
@@ -26,12 +41,159 @@ class pythonscript_configuration {
     }
 }
 
-class pythonscript_core { }
+class py_std_function {
+
+}
+
+class py_std_operators {
+    constructor() {
+        this.py_operators = {
+            "+": this.py_plus,
+            "-": this.py_minus,
+            "*": this.py_mupltyply,
+            "**": this.py_exponent,
+            "/": this.py_division,
+            "//": this.py_division_round,
+            "%": this.py_modulo,
+            "<<": this.py_undefined_function,
+            ">>": this.py_undefined_function,
+            "&": this.py_undefined_function,
+            "|": this.py_undefined_function,
+            "^": this.py_undefined_function,
+            "~": this.py_undefined_function,
+            "<": this.py_logic_less,
+            ">": this.py_logic_more,
+            "<=": this.py_logic_less_and_eq,
+            ">=": this.py_logic_more_and_eq,
+            "==": this.py_logic_equall,
+            "!=": this.py_logic_not_eq,
+            "not": this.py_logic_not,
+            "and": this.py_logic_and,
+            "or": this.py_logic_or
+        }
+    }
+
+    detect_operator(operator) {
+        return typeof this.py_operators[operator] !== "undefined";
+    }
+
+    run_opertor(operator, val_left, val_right) {
+        return this.py_operators[operator](val_left, val_right)
+    }
+
+    py_plus(val_left, val_right) {
+        try { var result = val_left + val_right; }
+        catch { throw ("Operator execution error '+'") }
+        return result;
+    }
+
+    py_minus(val_left, val_right) {
+        try { var result = val_left - val_right; }
+        catch { throw ("Operator execution error '-'") }
+        return result;
+    }
+
+    py_mupltyply(val_left, val_right) {
+        try { var result = val_left * val_right; }
+        catch { throw ("Operator execution error '*'") }
+        return result;
+    }
+
+    py_exponent(val_left, val_right) {
+        try { var result = Math.pow(val_left, val_right); }
+        catch { throw ("Operator execution error '**'") }
+        return result;
+    }
+
+    py_division(val_left, val_right) {
+        try { var result = val_left / val_right; }
+        catch { throw ("Operator execution error '/'") }
+        return result;
+    }
+
+    py_division_round(val_left, val_right) {
+        try { var result = (val_left / val_right) > 0 ? Math.floor(val_left / val_right) : Math.ceil(val_left / val_right); }
+        catch { throw ("Operator execution error '//'") }
+        return result;
+    }
+
+    py_modulo(val_left, val_right) {
+        try { var result = val_left % val_right; }
+        catch { throw ("Operator execution error '%'") }
+        return result;
+    }
+
+    py_undefined_function(val_left, val_right) {
+        throw ("Operator execution error 'undefined operator'")
+    }
+
+    py_logic_less(val_left, val_right) {
+        try { var result = val_left < val_right; }
+        catch { throw ("Operator execution error '<'") }
+        return result;
+    }
+
+    py_logic_more(val_left, val_right) {
+        try { var result = val_left > val_right; }
+        catch { throw ("Operator execution error '>'") }
+        return result;
+    }
+
+    py_logic_less_and_eq(val_left, val_right) {
+        try { var result = val_left <= val_right; }
+        catch { throw ("Operator execution error '<='") }
+        return result;
+    }
+
+    py_logic_more_and_eq(val_left, val_right) {
+        try { var result = val_left >= val_right; }
+        catch { throw ("Operator execution error '>='") }
+        return result;
+    }
+
+    py_logic_more_and_eq(val_left, val_right) {
+        try { var result = val_left == val_right; }
+        catch { throw ("Operator execution error '=='") }
+        return result;
+    }
+
+    py_logic_not(val_left, val_right) {
+        try { var result = val_left == false ? true : false; }
+        catch { throw ("Operator execution error 'not'") }
+        return result;
+    }
+
+    py_logic_and(val_left, val_right) {
+        try { var result = (val_left == true && val_right == true || val_left == false && val_right == false); }
+        catch { throw ("Operator execution error 'and'") }
+        return result;
+    }
+
+    py_logic_or(val_left, val_right) {
+        try { var result = (val_left == true || val_right == false || val_left == false && val_right == true); }
+        catch { throw ("Operator execution error 'and'") }
+        return result;
+    }
+}
+
+class pythonscript_core {
+    constructor () {
+        this.py_opertators = new py_std_operators();
+    }
+    load_conf() {
+        return -1;
+    }
+
+    run() {
+        return -1;
+    }
+}
 
 class pythonscript_controller {
     constructor(default_params) {
+        this.PS_conf = new pythonscript_configuration();
+
         if (!default_params) {
-            this.PS_conf = new pythonscript_configuration();
             this.PS_conf.set_default_params();
             return;
         }
@@ -39,13 +201,15 @@ class pythonscript_controller {
         if (!isNativeObject(default_params) || typeof default_params.PS_configuration == "undefined")
             throw ("Parameters must be NULL or pythonscript_configuration");
 
-        this.PS_runner = new pythonscript_core();
-
-        this.runner
+        this.PS_conf
     }
 
     run(code_block) {
-        return -1;
+        var ps_core = new pythonscript_core();
+
+        ps_core.load_conf(this.PS_conf);
+        ps_core.run(code_block);
+
     }
 }
 
@@ -62,9 +226,13 @@ if (__RUN_MODE__ == "__NODE__") {
     module.exports.pythonscript_core = pythonscript_core;
     module.exports.pythonscript_configuration = pythonscript_configuration;
     module.exports.__RUN_MODE__ = __RUN_MODE__
-    console.log("piska");
 }
 
 if (__RUN_MODE__ == "__HTML__") {
     console.log("loh");
+}
+
+py_opertators = new py_std_operators();
+if (py_opertators.detect_operator("+")) {
+    console.log(py_opertators.run_opertor("//", 7, 3));
 }
